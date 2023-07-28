851458e36261f633c863b8f528135abda641a7e0
Cyberghxst <alphadroidl20@gmail.com>
2023-07-27 18:17:24 -0600

    Updated type definitions

--- typings.d.ts
+++ typings.d.ts
@@ -10,18 +10,49 @@ export interface Command {
 }
 
 export declare class LoaderError extends Error {
+    /**
+     * @param title Error title.
+     * @param message Error message.
+     */
     constructor(title: string, message: string)
 }
 
 export declare class Loader {
+    /**
+     * 
+     * @param client aoi.js client definition.
+     * @param addToClient Adds the loader into the client.
+     */
     constructor(client: AoiClient, addToClient?: boolean);
+    /**
+     * Load commands into the client.
+     * @param dir Commands path.
+     */
     load(dir: string): Promise<void>;
+    /**
+     * Reload the commands.
+     */
     reload(): Promise<void>;
+    /**
+     * @private Add the command updater to the client.
+     */
     #addPlugin(): void;
+    /**
+     * @private Import a command.
+     */
     #import(root: string, dir: string, file: string): Promise<Command>;
+    /**
+     * @private Set the collected command array into the client.
+     * @param commands Array of collected commands.
+     */
     #set(commands: Command[]): void;
 }
 
 declare class Reader {
+    /**
+     * Parses a command source.
+     * @param input Command source.
+     * @returns 
+     */
     parse(input: string): Command
 }
